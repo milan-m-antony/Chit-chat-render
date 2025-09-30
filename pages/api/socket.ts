d61e74a59d27d5ddbd398c991bf07d768dbe47a1
Milan M Antony <milanmantony@gmail.com>
2025-09-30 09:55:35 +0530

    Fix Socket.IO types in API route

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { Server as NetServer } from 'http';
 import { SocketService } from '@/lib/socket';
+import { Server as SocketIOServer } from 'socket.io';
+
+// Extend the NextApiResponse type to include socket with server
+interface SocketNextApiResponse extends NextApiResponse {
+  socket: any;
+}
 
 export const config = {
   api: {
@@ -10,11 +16,13 @@ export const config = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: SocketNextApiResponse
 ) {
-  if (!res.socket?.server?.io) {
-    const httpServer: NetServer = res.socket.server as any;
+  if (!res.socket.server.io) {
+    console.log('Initializing Socket.IO server...');
+    const httpServer: NetServer = res.socket.server;
     await SocketService.initialize(httpServer);
+    console.log('Socket.IO server initialized');
   }
   res.end();
 }
